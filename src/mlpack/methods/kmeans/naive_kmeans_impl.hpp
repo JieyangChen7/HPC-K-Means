90f3a6c070ef7000456cb0bbdd8ff270eb65fec9
Jieyang Chen <cjy7117@gmail.com>
2016-03-14 20:51:11 -0700

    test

--- src/mlpack/methods/kmeans/naive_kmeans_impl.hpp
+++ src/mlpack/methods/kmeans/naive_kmeans_impl.hpp
@@ -30,12 +30,12 @@ NaiveKMeans<MetricType, MatType>::NaiveKMeans(const MatType& dataset,
     distanceCalculations(0)
 { 
   ddt.set_size(dataset.n_cols, 1);
-  arma::mat dataset_t = dataset.t();
-  for (size_t i = 0; i < dataset.n_cols; i++)
-  {
-    arma::mat temp = dataset_t.row(i) * dataset.col(i);
-    ddt(i, 1) = temp(0, 0);
-  }
+  // arma::mat dataset_t = dataset.t();
+  // for (size_t i = 0; i < dataset.n_cols; i++)
+  // {
+  //   arma::mat temp = dataset_t.row(i) * dataset.col(i);
+  //   ddt(i, 1) = temp(0, 0);
+  // }
 /* Nothing to do. */ }
 
 // Run a single iteration.
