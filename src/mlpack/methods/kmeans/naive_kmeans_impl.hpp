652a59f6c77c44d05cd69109625375e0a53c5652
Jieyang Chen <cjy7117@gmail.com>
2016-03-14 21:18:58 -0700

    test

--- src/mlpack/methods/kmeans/naive_kmeans_impl.hpp
+++ src/mlpack/methods/kmeans/naive_kmeans_impl.hpp
@@ -31,7 +31,7 @@ NaiveKMeans<MetricType, MatType>::NaiveKMeans(const MatType& dataset,
 { 
   
   ddt.set_size(dataset.n_cols, 1);
-  Log::Info << ddt.n_rows << std::endl;
+  Log::Info << ddt.n_rows << "-" << ddt.n_cols << std::endl;
   arma::mat dataset_t = dataset.t();
    for (size_t i = 0; i < dataset.n_cols; i++)
    {
